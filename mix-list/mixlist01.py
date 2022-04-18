2fb43d2c78546da357d04c904c535c15a8793751
Andrew Taylor <andrewtaylor91@users.noreply.github.com>
2022-04-18 20:58:17 +0000

    manipulating lists

---
+++ mix-list/mixlist01.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+
+iplist = [ 5060, "80", 55, "10.0.0.1", "10.20.30.1", "ssh" ]
+#Display first item in list
+print("The first IP is: " + iplist[3] )
+#Display third item in list
+print("The Second ip in the list: " + iplist[4] )
+
