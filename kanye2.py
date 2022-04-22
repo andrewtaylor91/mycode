eba374dec3fa260e449071a6ffe7945ef5772c97
Andrew Taylor <andrewtaylor91@users.noreply.github.com>
2022-04-22 21:39:31 +0000

    Kanye

--- kanye2.py
+++ kanye2.py
@@ -7,19 +7,24 @@ kanye_url = 'https://api.kanye.rest'
 
 print(crayons.yellow("\nThe Gospel According to Kanye West\n"))
 
+def getquote():
+    resp = requests.get(kanye_url)
+    kanye_quote = resp.json()
+    quote = kanye_quote["quote"]
+    return quote
+
+
 def main():
     
     while True:    
              
         answer1 = input("Would you like to hear a quote from the Gospel of Kanye West? y/n ")
         if answer1 == "y":
-            resp = requests.get(kanye_url)
-            kanye_quote = resp.json()
-            quote = kanye_quote["quote"]
-            print(crayons.red(quote))
+            
+            print(crayons.red(getquote()))
             answer2 = input("\n Would you like to hear another? y/n ")
             if answer2 == "y":
-                print(crayons.red(requests.get(kanye_url.json())))
+                print(crayons.red(getquote()))
             elif answer2 == "n":
                 print("\nGoodbye")
                 break
